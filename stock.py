3dc3310a533620b045ee3a5c84daa054ce4096ea
ltpjob <ltpjob@163.com>
2017-09-20 13:33:08 +0800

    【mod】summary+softmax

--- stock.py
+++ stock.py
@@ -62,10 +62,22 @@ def add_layer(inputs,
 
 def dense(x,
           size,
+          phase,
+          keep_prob,
           scope):
-    return tf.contrib.layers.fully_connected(x, size,
-                                             activation_fn=None,
-                                             scope=scope)
+    with tf.variable_scope(scope):
+        up = tf.contrib.layers.fully_connected(x, size,
+                                               activation_fn=None,
+                                               scope='dense')
+
+        # up = tf.contrib.layers.batch_norm(up,
+        #                               center=True, scale=True,
+        #                               is_training=phase,
+        #                               scope='bn')
+
+        up = tf.nn.dropout(up, keep_prob, name="do")
+
+    return up
 
 
 def dense_relu(x, size, keep_prob, scope):
@@ -77,14 +89,16 @@ def dense_relu(x, size, keep_prob, scope):
 
 def dense_batch_relu(x, size, phase, keep_prob, scope):
     with tf.variable_scope(scope):
-        h1 = tf.contrib.layers.fully_connected(x, size, activation_fn=None, scope='dense')
-        drop = tf.nn.dropout(h1, keep_prob, name="do")
-        h2 = tf.contrib.layers.batch_norm(drop,
-                                          center=True, scale=True,
-                                          is_training=phase,
-                                          scope='bn')
+        up = tf.contrib.layers.fully_connected(x, size, activation_fn=None, scope='dense')
 
-        return tf.nn.relu(h2, 'relu')
+        # up = tf.contrib.layers.batch_norm(up,
+        #                                   center=True, scale=True,
+        #                                   is_training=phase,
+        #                                   scope='bn')
+
+        up = tf.nn.dropout(up, keep_prob, name="do")
+
+        return tf.nn.tanh(up, 'tanh')
 
 
 def one_hot_matrix(labels,
@@ -156,8 +170,9 @@ def main():
     data = np.load("data.npy")
     target = np.load("target.npy")
 
-    hidden_size = 4
+    hidden_size = 5
     learning_rate = 0.1
+    dropout_keep = 0.7
     class_num = 2
 
     num_epochs = 2500000
@@ -174,15 +189,15 @@ def main():
     # print(train_data.shape, train_labels.shape)
 
     # data_set = data_get_whatever(data, target)
-    data_set = data_get_group(data, target, 1, 17)
+    data_set = data_get_group(data, target, 1, 19)
 
     train_data = data_set["train_data"]
     train_labels = data_set["train_labels"]
     test_data = data_set["test_data"]
     test_labels = data_set["test_labels"]
 
-    X = tf.placeholder(tf.float32, shape=(None, train_data.shape[1]))
-    Y = tf.placeholder(tf.int32, shape=(None, class_num))
+    X = tf.placeholder(tf.float32, shape=(None, train_data.shape[1]), name="X")
+    Y = tf.placeholder(tf.int32, shape=(None, class_num), name='Y')
     phase = tf.placeholder(tf.bool, name='phase')
     keep_prob = tf.placeholder(tf.float32)
 
@@ -195,41 +210,52 @@ def main():
     #
     # logits = add_layer(input_layer, input_size, class_num, activation_function=None)
 
-    output_size = int(train_data.shape[1])
+    output_size = int(train_data.shape[1]*3)
     print(output_size)
     for i in range(hidden_size):
         input_layer = dense_batch_relu(input_layer, output_size, phase, keep_prob, "layer"+str(i+i))
 
-    logits = dense(input_layer, class_num, "layer" + str(i + i))
+    logits = dense(input_layer, class_num, phase, keep_prob, "layer_final")
 
     cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels=Y, logits=logits, name='xentropy')
     cost = tf.reduce_mean(cross_entropy, name='xentropy_mean')
+    tf.summary.scalar('loss', cost)
     optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(cost)
+
+    correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(Y, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    tf.summary.scalar('accuracy', accuracy)
 
     init = tf.global_variables_initializer()
 
     with tf.Session() as sess:
         sess.run(init)
+        merged = tf.summary.merge_all()
+        train_writer = tf.summary.FileWriter("logs/train_%d_%f_%f"%(hidden_size, learning_rate, dropout_keep), sess.graph)
+        test_writer = tf.summary.FileWriter("logs/test_%d_%f_%f"%(hidden_size, learning_rate, dropout_keep), sess.graph)
 
         for epoch in range(num_epochs):
             start_time = time.time()
             _, loss_value = sess.run([optimizer, cost], feed_dict={X: train_data, Y: train_labels,
-                                                                   phase:True, keep_prob:0.3})
+                                                                   phase:False, keep_prob:dropout_keep})
             duration = time.time() - start_time
             # print('Step %d: loss = %.8f (%.3f sec)' % (epoch, loss_value, duration))
 
             # Print the cost every epoch
             if epoch % 100 == 0:
                 # print('Step %d: loss = %.8f (%.3f sec)' % (epoch, loss_value, duration))
-                correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(Y, 1))
-                accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
                 # print ("train loss:%.8f Train Accuracy:"%(loss_value), accuracy.eval({X: train_data, Y: train_labels,
                 #                                                                       phase:True, keep_prob:1}))
-                loss_value = sess.run(cost, feed_dict={X: test_data, Y: test_labels,
-                                                       phase:False, keep_prob:1})
-                print("test loss:%.8f Test Accuracy:"%(loss_value), accuracy.eval({X: test_data, Y: test_labels,
+
+                train_result, loss_train = sess.run([merged, cost], feed_dict={X: train_data, Y: train_labels, phase: False, keep_prob: 1})
+                acc_train = accuracy.eval({X: train_data, Y: train_labels, phase:False, keep_prob:1})
+                test_result, loss_test = sess.run([merged, cost], feed_dict={X: test_data, Y: test_labels, phase:False, keep_prob:1})
+                acc_test = accuracy.eval({X: test_data, Y: test_labels, phase: False, keep_prob: 1})
+                print("Test loss:%.8f Test Accuracy:%.8f  |  Train loss:%.8f Train Accuracy:%.8f  epoch:%d"
+                      %(loss_test, acc_test, loss_train, acc_train, epoch))
+                train_writer.add_summary(train_result, epoch)
+                test_writer.add_summary(test_result, epoch)
 
 
-
-                                                                                   phase:False, keep_prob:1}))
 main()
